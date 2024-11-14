7dba2eea5d33136425f3423dbbb8da5c66062d77
Ondřej Zmatlík <ozmatlik@gmail.com>
2024-11-14 12:54:11 +0100

    Add task restoration feature and data protection support
    Introduced a new feature for restoring deleted tasks in Home.razor, including UI adjustments and backend support. Implemented IDataProtectionKeyContext in ApplicationDbContext and added DbSet<DataProtectionKey> for managing data protection keys. Updated Program.cs to configure data protection services. Added new package references and updated existing ones in Tasker.Web.csproj. Updated Dockerfile for better user management. Created a new migration to add the DataProtectionKeys table.

--- Tasker.Web/Data/ApplicationDbContext.cs
+++ Tasker.Web/Data/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Tasker.Web.Data.Entities;
 
 namespace Tasker.Web.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>(options), IDataProtectionKeyContext
     {
+        public DbSet<DataProtectionKey> DataProtectionKeys => Set<DataProtectionKey>();
         public DbSet<Group> Groups => Set<Group>();
         public DbSet<SchoolTask> Tasks => Set<SchoolTask>();
         public DbSet<Subject> Subjects => Set<Subject>();
         public DbSet<TaskType> TaskTypes => Set<TaskType>();
+
     }
 }
