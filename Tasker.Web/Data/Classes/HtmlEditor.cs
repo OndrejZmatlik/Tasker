6af8a8671b7dbbf019e9673b84e3fdee38103d41
Ondřej Zmatlík <ozmatlik@gmail.com>
2026-04-25 18:34:32 +0200

    Add rich text editor for assignment descriptions
    Replaced plain text field with MudExRichTextEditor in Add/Edit assignment page. Added HtmlEditor helper for toolbar config. Assignment descriptions are now sanitized with HtmlSanitizer before rendering to prevent XSS. Updated project dependencies to include MudExRichTextEditor and HtmlSanitizer. Minor refactoring for editor loading and sanitizer instantiation.

---
+++ Tasker.Web/Data/Classes/HtmlEditor.cs
@@ -0,0 +1,28 @@
+﻿using MudExRichTextEditor.Types;
+
+namespace Tasker.Web.Data.Classes
+{
+    public class HtmlEditor
+    {
+        public static IEnumerable<QuillTool> All()
+        {
+            yield return new QuillTool("ql-header", "", 1, new string[7] { "", "1", "2", "3", "4", "5", "6" });
+            yield return new QuillTool("ql-bold", "", 2);
+            yield return new QuillTool("ql-italic", "", 2);
+            yield return new QuillTool("ql-underline", "", 2);
+            yield return new QuillTool("ql-strike", "", 2);
+            yield return new QuillTool("ql-color", "", 3, Array.Empty<string>());
+            yield return new QuillTool("ql-background", "", 3, Array.Empty<string>());
+            yield return new QuillTool("ql-list", "ordered", 4);
+            yield return new QuillTool("ql-list", "bullet", 4);
+            yield return new QuillTool("ql-indent", "-1", 4);
+            yield return new QuillTool("ql-indent", "+1", 4);
+            yield return new QuillTool("ql-align", "", 4, new string[4] { "", "center", "right", "justify" });
+            yield return new QuillTool("ql-blockquote", "", 5);
+            //yield return new QuillTool("ql-code-block", "", 5);
+            yield return new QuillTool("ql-link", "", 6);
+            //yield return new QuillTool("ql-image", "", 6);
+            //yield return new QuillTool("ql-video", "", 6);
+        }
+    }
+}
